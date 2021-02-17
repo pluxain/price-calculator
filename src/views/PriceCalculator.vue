a294809f8ff14b7c4a027a68e2c114214733ee2d
Pascal Luxain <pascalln.dev@gmail.com>
2021-02-17 09:51:51 +0100

    Add basic form placeholder

--- src/views/PriceCalculator.vue
+++ src/views/PriceCalculator.vue
@@ -1,5 +1,26 @@
 <template>
   <section>
     <h1>Price Calculator</h1>
+
+    <form>
+      <fieldset>
+        <legend>Price calculator</legend>
+      </fieldset>
+    </form>
   </section>
 </template>
+
+<style scoped>
+section {
+  @apply flex flex-col items-center;
+}
+form {
+  @apply w-1/3 flex flex-row justify-center;
+}
+fieldset {
+  @apply border flex w-full p-2;
+}
+legend {
+  @apply px-2 text-left;
+}
+</style>
