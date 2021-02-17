42782cb1235aa542bdc42ac2427572c8c49dfdf9
Pascal Luxain <pascalln.dev@gmail.com>
2021-02-17 09:39:18 +0100

    clean up and code style setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <div id="app"></div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld
-  }
+  name: "App"
 });
 </script>
 
