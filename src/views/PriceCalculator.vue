f7eaf01064ad4af354097046cd6d9c6f9012f130
Pascal Luxain <pascalln.dev@gmail.com>
2021-02-17 09:58:59 +0100

    Add baseprice field

--- src/views/PriceCalculator.vue
+++ src/views/PriceCalculator.vue
@@ -5,11 +5,32 @@
     <form>
       <fieldset>
         <legend>Price calculator</legend>
+        <div class="field-input">
+          <label for="basePrice" class="label">Baseprice</label>
+          <input
+            type="number"
+            class="input"
+            step="0.01"
+            min="0"
+            id="basePrice"
+            v-model="basePrice"
+          />
+        </div>
       </fieldset>
     </form>
   </section>
 </template>
 
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      basePrice: 0.0
+    };
+  }
+});
+</script>
 <style scoped>
 section {
   @apply flex flex-col items-center;
@@ -22,5 +43,14 @@ fieldset {
 }
 legend {
   @apply px-2 text-left;
+}
+.field-input {
+  @apply flex flex-row w-full;
+}
+.label {
+  @apply w-1/2 text-left px-2;
+}
+.input {
+  @apply border flex-grow ml-2 px-1;
 }
 </style>
